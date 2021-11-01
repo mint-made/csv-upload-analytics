b757aea376b806313a817b3ecd7d5e3c327c98b9
mint-made <thomaskupai@gmail.com>
2021-11-01 00:42:45 +0000

    layout improved

--- frontend/src/screens/UploadScreen.js
+++ frontend/src/screens/UploadScreen.js
@@ -33,54 +33,69 @@ const UploadScreen = () => {
     }
   };
   return (
-    <div>
-      <Form>
-        <Card>
-          <Form.Group controlId='csv-file' className='mb-5 mx-auto'>
-            <Form.Label>CSV File</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Filename'
-              value={file}
-              onChange={(e) => setFile(e.target.value)}
-            ></Form.Control>
-            <Form.File
-              id='csvFile'
-              label='CSV File'
-              custom
-              onChange={uploadFileHandler}
-            ></Form.File>
-          </Form.Group>
+    <>
+      <div className='d-flex justify-content-around align-items-center mb-5 mx-auto'>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img
+            variant='top'
+            src='https://user-images.githubusercontent.com/53815961/139601273-9e9e9dd4-e7e7-4cef-8e4a-4c1ccc2fc650.png'
+          />
+          <Card.Body>
+            <Card.Title>CSV file format</Card.Title>
+            <Card.Text>
+              To generate a report analysing integers in a .csv file, be sure to
+              have a single column of numbers and ensure the header has the word
+              "number"
+            </Card.Text>
+          </Card.Body>
         </Card>
-        {uploading ? (
-          <Spinner animation='border' role='status'>
-            <span className='visually-hidden'>Loading...</span>
-          </Spinner>
-        ) : statisticHeaders.length > 0 && statisticValues.length > 0 ? (
-          <div className='d-flex justify-content-center'>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  {statisticHeaders.map((header, index) => (
-                    <th key={index}>{header}</th>
-                  ))}
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  {statisticValues.map((value, index) => (
-                    <td key={index}>{value}</td>
-                  ))}
-                </tr>
-              </tbody>
-            </Table>
-          </div>
-        ) : (
-          <></>
-        )}
-      </Form>
-    </div>
+        <Form>
+          <Card>
+            <Form.Group controlId='csv-file'>
+              <Form.Label>CSV File</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Filename'
+                value={file}
+                onChange={(e) => setFile(e.target.value)}
+              ></Form.Control>
+              <Form.File
+                id='csvFile'
+                label='CSV File'
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+            </Form.Group>
+          </Card>
+        </Form>
+      </div>
+      {uploading ? (
+        <Spinner animation='border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      ) : statisticHeaders.length > 0 && statisticValues.length > 0 ? (
+        <div className='d-flex justify-content-center p-5'>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                {statisticHeaders.map((header, index) => (
+                  <th key={index}>{header}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {statisticValues.map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
